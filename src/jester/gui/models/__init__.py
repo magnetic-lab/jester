c3f5e9dc7027431f9d39df6084dc3c54e90679fd
Jacob Martinez <jacobmartinez3d@gmail.com>
2024-01-15 11:11:20 +0800

    refactor: rename model

--- src/jester/gui/models/__init__.py
+++ src/jester/gui/models/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .filesystem_model import JesterFileSystemModel
 from .drives_list_model import JesterDrivesListModel
-from .turnover_graph_model import JesterTurnoverGraphModel
+from .graph_model import JesterTurnoverGraphModel
 from .project_properties_treeview_model import ProjectPropertiesTreeViewModel
+from .copy_list_model import JesterCopyListModel
